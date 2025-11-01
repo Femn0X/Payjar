57679768ad279da50e43f7abe9be05e0e91ca0a0
Femn0X <lucawinecker@gmail.com>
2025-11-01 22:47:53 +0100

    update repo structure

--- test_init.py
+++
@@ -1,8 +0,0 @@
-from Payjar.ui import *
-def createWindow_test(w,h):
-  Window(w,h)
-createWindow_test(1000,1000)
-def test_runner(code):
-  try:PJRT(code)
-  except Exception as e:
-    print(e)
---
+++ tests/test_init.py
@@ -0,0 +1,16 @@
+from Payjar.ui import Window, PJRT  # absolute import works fine in GitHub Actions
+
+def createWindow_test(w, h):
+    Window(w, h)
+
+def test_runner(code):
+    try:
+        PJRT(code)
+    except Exception as e:
+        print(e)
+
+def test_window_creation():
+    createWindow_test(1000, 1000)
+
+def test_runner_failure():
+    test_runner("fail")  # should print "PJRT error"
